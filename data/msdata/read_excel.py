b1c692f541bc981ea2439397986dce30ff65f87e
haskoe <henrik@haskoe.dk>
2022-04-08 20:37:20 +0200

    find rækker hvor mindst 1 UniquePeptides kolonne er >1

--- data/msdata/read_excel.py
+++ data/msdata/read_excel.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 script_dir = path.dirname(path.realpath(__file__))
 fname = 'Batch2_data.csv'
-data = pd.read_csv(path.join(script_dir, fname),header='infer')
+data = pd.read_csv(path.join(script_dir, fname), header='infer')
 
-idxs = [i for i,c in enumerate(data.columns) if c.find('UniquePeptides')>0]
-header = data.values[0]
-l = len(data)
-
+idxs = [i for i, c in enumerate(data.columns) if c.find('UniquePeptides') > 0]
+rng = range(len(data.values))
+valid_rows = [
+    r for r in rng
+    if [idx for idx in idxs if data.values[r][idx] > 1]
+]
