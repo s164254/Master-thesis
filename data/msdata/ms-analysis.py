91989523f11bb4d00a90748e5d3d5fa0786e5085
haskoe <henrik@haskoe.dk>
2022-04-20 21:20:22 +0200

    base common_proteins and fold_change helper

--- data/msdata/ms-analysis.py
+++ data/msdata/ms-analysis.py
@@ -26,6 +26,10 @@ def write_csv(csv_file_name,header,data):
         for row in data:
             wrtr.writerow(row)
 
+def common_proteins_criteria(v1,v2):
+    return v1>0 and v2>0
+
+
 def is_fold(v1,v2,n):
     ma = max(v1,v2)
     if ma == 0:
@@ -64,22 +68,18 @@ class MsAnalysis:
             item_data[item_name][attr_name] = tuple([parse(row[i]) for row in data])
         self.item_data = item_data
 
-    def common_proteins(self,item_names,attr_name):
-        item1_values,item2_values = [self.item_data[item_name][attr_name] for item_name in item_names]
-        return [(self.UniprotIds[i],self.Genes[i],self.ProteinDescriptions[i],item1_values[i],item2_values[i]) for i in range(len(self.Genes)) if item1_values[i]>0 and item2_values[i]>0]
-
-    def fold_change(self,item_names,attr_name):
+    def helper(self,item_names,attr_name,criteria):
         item1,item2 = item_names
         v1 = self.item_data[item1][attr_name]
         v2 = self.item_data[item2][attr_name]
-        temp = [(i,v1[i],v2[i]) for i in range(len(self.Genes))]
-        temp = [(i,v1,v2) for i,v1,v2 in temp if is_two_fold(v1,v2)]
+        temp = [(i,v1[i],v2[i]) for i in range(len(self.Genes)) if criteria(v1[i],v2[i])]
         return [(self.UniprotIds[i],self.Genes[i],self.ProteinDescriptions[i],v1,v2) for i,v1,v2 in temp]
 
-        common_proteins = m.common_proteins(item_names,attr_name)
+    def common_proteins(self,item_names,attr_name):
+        return self.helper(item_names,attr_name,common_proteins_criteria)
 
-        item1_values,item2_values = [self.item_data[item_name][attr_name] for item_name in item_names]
-        return [(self.UniprotIds[i],self.Genes[i],self.ProteinDescriptions[i],item1_values[i],item2_values[i]) for i in range(len(self.Genes)) if item1_values[i]>0 and item2_values[i]>0]
+    def fold_change(self,item_names,attr_name):
+        return self.helper(item_names,attr_name,is_two_fold)
 
 script_dir = path.dirname(path.realpath(__file__))
 fname = 'Batch2_data.output.csv'
