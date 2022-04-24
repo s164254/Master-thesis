35331fa5b15cc89683c4dd26680c45b812f382e2
haskoe <henrik@haskoe.dk>
2022-04-24 19:38:06 +0200

    rettelser

--- data/msdata/ms-analysis.py
+++ data/msdata/ms-analysis.py
@@ -27,6 +27,7 @@ def is_two_fold(v1, v2):
 
 
 UNIQUE_PEPTIDES = 'UniquePeptides'
+LABEL_FREE_QUANT = 'Label-Free Quant'
 
 
 class MsAnalysis:
@@ -63,6 +64,29 @@ class MsAnalysis:
         ]
         l2 = len(rows)
 
+        item_data = {}
+        for col_index, item_name, attr_name in self.item_attrs:
+            if attr_name == UNIQUE_PEPTIDES:
+                continue
+
+            if not item_name in item_data:
+                item_data[item_name] = {}
+
+            NOT_VALID_VALUE = 0
+            unique_peptides_offset = self.get_offset(col_index, attr_name,
+                                                     item_name,
+                                                     UNIQUE_PEPTIDES)
+
+            for row_index, row in enumerate(rows):
+                if not utils.unique_peptides_valid_value(
+                        row[col_index + unique_peptides_offset]):
+                    row[col_index] = '0'
+
+            item_data[item_name][attr_name] = tuple([
+                utils.parse_csv_cell(row[col_index], NOT_VALID_VALUE)
+                for row in rows
+            ])
+
         self.UniprotIds = tuple([row[0] for row in rows])
         self.Genes = tuple([row[1] for row in rows])
         self.ProteinDescriptions = tuple([row[1] for row in rows])
@@ -71,7 +95,7 @@ class MsAnalysis:
         for col_index, item_name, attr_name in self.item_attrs:
             if attr_name == UNIQUE_PEPTIDES:
                 continue
-            
+
             if not item_name in item_data:
                 item_data[item_name] = {}
 
@@ -79,12 +103,7 @@ class MsAnalysis:
             unique_peptides_offset = self.get_offset(col_index, attr_name,
                                                      item_name,
                                                      UNIQUE_PEPTIDES)
-            item_data[item_name][attr_name] = tuple([
-                utils.unique_peptides_valid_value(
-                    row[col_index + unique_peptides_offset])
-                and utils.parse_csv_cell(row[col_index], NOT_VALID_VALUE)
-                or NOT_VALID_VALUE for row in rows
-            ])
+
         self.rows = rows
         self.item_data = item_data
 
@@ -118,14 +137,25 @@ class MsAnalysis:
     def fold_change(self, item_names, attr_name):
         return self.filter_helper(item_names, attr_name, is_two_fold)
 
+    def save_csv(self, csv_file_name, attr_name):
+        first_col_index = self.attr_first_column_index[attr_name]
+        item_name_len = len(self.item_names)
+
+        ft.write_csv(
+            csv_file_name, self.header[:3] +
+            self.header[first_col_index:first_col_index + item_name_len], [
+                row[:3] + row[first_col_index:first_col_index + item_name_len]
+                for row in self.rows
+            ])
+
 
 script_dir = ft.get_script_dir(__file__)
 fname = 'Batch2_data.csv'
 
 m = MsAnalysis(path.join(script_dir, fname))
-
+m.save_csv(path.join(script_dir, 'csv.csv'), LABEL_FREE_QUANT)
 item_names = ['M2', 'P2']
-common_proteins = m.common_proteins(item_names, 'Label-Free Quant')
-fold_change = m.fold_change(item_names, 'Label-Free Quant')
+common_proteins = m.common_proteins(item_names, LABEL_FREE_QUANT)
+fold_change = m.fold_change(item_names, LABEL_FREE_QUANT)
 ft.write_csv(path.join(script_dir, 'csv.csv'), m.header[:3] + item_names,
              common_proteins)
