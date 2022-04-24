ccd16cbf01713bd4547419cb2046ef0ef82a2ae3
haskoe <henrik@haskoe.dk>
2022-04-24 17:09:08 +0200

    ms-analysis withvalid row filter and fill zero if invalid unique peptides cell

--- data/msdata/ms-analysis.py
+++ data/msdata/ms-analysis.py
@@ -1,91 +1,131 @@
 from os import path
 import re
-import csv
+import fileutils as ft
+import utils
 
 ATTR_RE = '_([a-zA-Z0-9]+)\.raw\.PG\.(.+)'
 
-#c = '20220401_KMJ_LB_PB_140min_15cm_M2.raw.PG.PSMs'
-#m = re.search(ATTR_RE,c)
-def parse(cell_value):
-    if cell_value.find(';')>=0:
-        return tuple([parse(v) for v in cell_value.split(';')])
 
-    temp = cell_value.strip()
-    if not temp:
-        return -1
-    
-    if cell_value[-1] == '%':
-        return float(cell_value[:-1])
-    
-    return float(cell_value)
-
-def write_csv(csv_file_name,header,data):
-    with open(csv_file_name, 'w') as csvfile:
-        wrtr = csv.writer(csvfile, delimiter=',')
-        wrtr.writerow(header)
-        for row in data:
-            wrtr.writerow(row)
-
-def common_proteins_criteria(v1,v2):
-    return v1>0 and v2>0
+def common_proteins_criteria(v1, v2):
+    return v1 > 0 and v2 > 0
 
 
-def is_fold(v1,v2,n):
-    ma = max(v1,v2)
+def is_fold(v1, v2, n):
+    ma = max(v1, v2)
     if ma == 0:
         return False
 
-    mi = min(v1,v2)
+    mi = min(v1, v2)
     if mi == 0:
-        return True
+        return False  #True
 
-    return ma/mi >= 2
+    return ma / mi >= 2
 
 
-def is_two_fold(v1,v2):
-    return is_fold(v1,v2,2)
+def is_two_fold(v1, v2):
+    return is_fold(v1, v2, 2)
+
+
+UNIQUE_PEPTIDES = 'UniquePeptides'
+
 
 class MsAnalysis:
+
     def __init__(self, csv_file_name):
-        with open(path.join(script_dir, csv_file_name)) as csvfile:
-            rdr = csv.reader(csvfile, delimiter=',')
-            rows = [row for row in rdr]
+        self.header, rows = ft.load_csv(csv_file_name, ',', True)
 
-        self.header = rows[0]
-        data = rows[1:]
-        item_attrs = [(i,m.groups()[0],m.groups()[1]) for i,m in [(i,re.search(ATTR_RE,c)) for i,c in enumerate(self.header)] if m and len(m.groups())==2]
-        self.attrs = tuple(set([attr_name for i,item_name,attr_name in item_attrs]))
-        self.item_names = tuple(set([item_name for i,item_name,attr_name in item_attrs]))
+        item_attrs = [(i, m.groups()[0], m.groups()[1])
+                      for i, m in [(i, re.search(ATTR_RE, c))
+                                   for i, c in enumerate(self.header)]
+                      if m and len(m.groups()) == 2]
+        self.attrs = tuple(
+            set([attr_name for i, item_name, attr_name in item_attrs]))
+        self.attr_first_column_index = dict([
+            (attr,
+             min([
+                 i for i, item_name, attr_name in item_attrs
+                 if attr_name == attr
+             ])) for attr in self.attrs
+        ])
+        unique_peptides_first_column_index = self.attr_first_column_index[
+            UNIQUE_PEPTIDES]
+        self.item_names = tuple(
+            set([item_name for i, item_name, attr_name in item_attrs]))
+        self.item_attrs = item_attrs
 
-        self.UniprotIds = tuple([row[0] for row in data])
-        self.Genes = tuple([row[1] for row in data])
-        self.ProteinDescriptions = tuple([row[1] for row in data])
+        item_name_len = len(self.item_names)
+        l1 = len(rows)
+        # remove 'empty' rows
+        rows = [
+            row for row in rows if utils.unique_peptides_valid_row(
+                row[unique_peptides_first_column_index:
+                    unique_peptides_first_column_index + item_name_len])
+        ]
+        l2 = len(rows)
+
+        self.UniprotIds = tuple([row[0] for row in rows])
+        self.Genes = tuple([row[1] for row in rows])
+        self.ProteinDescriptions = tuple([row[1] for row in rows])
 
         item_data = {}
-        for i,item_name,attr_name in item_attrs:
+        for col_index, item_name, attr_name in self.item_attrs:
+            if attr_name == UNIQUE_PEPTIDES:
+                continue
+            
             if not item_name in item_data:
                 item_data[item_name] = {}
-            item_data[item_name][attr_name] = tuple([parse(row[i]) for row in data])
+
+            NOT_VALID_VALUE = 0
+            unique_peptides_offset = self.get_offset(col_index, attr_name,
+                                                     item_name,
+                                                     UNIQUE_PEPTIDES)
+            item_data[item_name][attr_name] = tuple([
+                utils.unique_peptides_valid_value(
+                    row[col_index + unique_peptides_offset])
+                and utils.parse_csv_cell(row[col_index], NOT_VALID_VALUE)
+                or NOT_VALID_VALUE for row in rows
+            ])
+        self.rows = rows
         self.item_data = item_data
 
-    def helper(self,item_names,attr_name,criteria):
-        item1,item2 = item_names
+    def get_offset(self, from_col_index, from_attr_name, from_item_name,
+                   to_attr_name_):
+        return [
+            to_col_index - from_col_index
+            for to_col_index, to_item_name, to_attr_name in self.item_attrs
+            if to_attr_name == to_attr_name_ and to_item_name == from_item_name
+        ][0]
+
+    def unique_peptides_valid_row_criteria(self, row):
+        return utils.unique_peptides_valid_row(
+            row[self.unique_peptides_first_column_index:self.
+                unique_peptides_first_column_index + len(self.item_names)])
+        #column_values = row[]
+
+    def filter_helper(self, item_names, attr_name, criteria):
+        item1, item2 = item_names
         v1 = self.item_data[item1][attr_name]
         v2 = self.item_data[item2][attr_name]
-        temp = [(i,v1[i],v2[i]) for i in range(len(self.Genes)) if criteria(v1[i],v2[i])]
-        return [(self.UniprotIds[i],self.Genes[i],self.ProteinDescriptions[i],v1,v2) for i,v1,v2 in temp]
+        temp = [(i, v1[i], v2[i]) for i in range(len(self.Genes))
+                if criteria(v1[i], v2[i])]
+        return [(self.UniprotIds[i], self.Genes[i],
+                 self.ProteinDescriptions[i], v1, v2) for i, v1, v2 in temp]
 
-    def common_proteins(self,item_names,attr_name):
-        return self.helper(item_names,attr_name,common_proteins_criteria)
+    def common_proteins(self, item_names, attr_name):
+        return self.filter_helper(item_names, attr_name,
+                                  common_proteins_criteria)
 
-    def fold_change(self,item_names,attr_name):
-        return self.helper(item_names,attr_name,is_two_fold)
+    def fold_change(self, item_names, attr_name):
+        return self.filter_helper(item_names, attr_name, is_two_fold)
 
-script_dir = path.dirname(path.realpath(__file__))
-fname = 'Batch2_data.output.csv'
+
+script_dir = ft.get_script_dir(__file__)
+fname = 'Batch2_data.csv'
 
 m = MsAnalysis(path.join(script_dir, fname))
-item_names = ['M2','P2']
-common_proteins = m.common_proteins(item_names,'Label-Free Quant')
-fold_change = m.fold_change(item_names,'Label-Free Quant')
-write_csv(path.join(script_dir, 'csv.csv'),m.header[:3]+item_names,common_proteins)
+
+item_names = ['M2', 'P2']
+common_proteins = m.common_proteins(item_names, 'Label-Free Quant')
+fold_change = m.fold_change(item_names, 'Label-Free Quant')
+ft.write_csv(path.join(script_dir, 'csv.csv'), m.header[:3] + item_names,
+             common_proteins)
