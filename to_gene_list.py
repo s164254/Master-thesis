e139efaaeb66ea05e1f082105ae26518325c6f0e
haskoe <henrik@haskoe.dk>
2022-06-03 19:29:43 +0200

    rename

--- to_gene_list.py
+++ to_gene_list.py
@@ -2,6 +2,6 @@ import csvtopandas
 import warnings
 warnings.filterwarnings("ignore")
 
-for e in ('proteomics_experiment1','proteomics_experiment2'):
+for e in ('proteomics_experiment_1','proteomics_experiment_2'):
     exp = csvtopandas.CsvToPandas(e)
     exp.to_gene_list()
