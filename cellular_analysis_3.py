d49734cf023e618a849f64e6a78eca353a919482
s164254 <s164254@github.com>
2022-06-09 18:51:01 +0200

    title closure

--- cellular_analysis_3.py
+++ cellular_analysis_3.py
@@ -2,12 +2,17 @@ import csvtopandas
 
 exp = csvtopandas.CsvToPandas('proteomics_experiment_2')
 all_sample_groups = (
-    ('Fa1a_500ng', 'Fa2a_500ng'),
-    ('Fa1a_500ng', 'T1_500ng', 'T2_500ng', 'T3_500ng'),
+    (('Fa1a_500ng', 'Fa2a_500ng'), 'Title 1'),
+    (('Fa1a_500ng', 'T1_500ng', 'T2_500ng', 'T3_500ng'), 'Title 2'),
 )
 
 def title_func(experiment_name, display_columns): 
     return 'Cellular analysis plot. Experiment:%s, sample names:%s' % (experiment_name,', '.join(display_columns))
 
-for sample_names in all_sample_groups:
-    exp.cellular_analysis_3(sample_names, title_func, 'X label', 'Y label')
+def title_closure(title): 
+    def f(experiment_name, display_columns):
+        return title
+    return f
+
+for sample_names, chart_title in all_sample_groups:
+    exp.cellular_analysis_3(sample_names, title_closure(chart_title), 'X label', 'Y label')
