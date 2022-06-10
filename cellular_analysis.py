de17b046598d90bfd5860a0cd452300b40875d01
Philip <s164254@student.dtu.dk>
2022-06-10 19:45:34 +0200

    labels

--- cellular_analysis.py
+++ cellular_analysis.py
@@ -2,5 +2,6 @@ import csvtopandas
 
 N = 20
 exp = csvtopandas.CsvToPandas('proteomics_experiment_1')
-exp.cellular_analysis(N, 'a title', 'bla bla', 'bli bli', ('M1', 'P1'))
-exp.cellular_analysis(N, 'a title', 'bla bla', 'bli bli', ('M1', 'M2'))
+exp.cellular_analysis(N, 'Abundance of selected cellular proteins for the samples', 'Gene names', 'Abundance', ('M1', 'P1'))
+exp.cellular_analysis(N, 'Abundance of selected cellular proteins for the samples', 'Gene names', 'Abundance', ('M1', 'M2'))
+
--- cellular_analysis_3.py
+++ cellular_analysis_3.py
@@ -2,8 +2,9 @@ import csvtopandas
 
 exp = csvtopandas.CsvToPandas('proteomics_experiment_2')
 all_sample_groups = (
-    (('Fa1a_500ng', 'Fa2a_500ng'), 'Title 1'),
-    (('Fa1a_500ng', 'T1_500ng', 'T2_500ng', 'T3_500ng'), 'Title 2'),
+    (('Fa1a_500ng', 'Fa2a_500ng'), 'Abundance of most abundant cellular proteins for the samples'),
+    (('T1_500ng', 'T2_500ng', 'T3_500ng'), 'Abundance of most abundant cellular proteins for the samples'),
+     (('Fa1a_500ng', 'T3_500ng'), 'Abundance of most abundant cellular proteins for the samples'),
 )
 
 def title_func(experiment_name, display_columns): 
@@ -15,4 +16,7 @@ def title_closure(title):
     return f
 
 for sample_names, chart_title in all_sample_groups:
-    exp.cellular_analysis_3(sample_names, title_closure(chart_title), 'X label', 'Y label')
+    exp.cellular_analysis_3(sample_names, title_closure(chart_title), 'Gene names', 'Abundance')
+
+
+#Have option to remove gene names from plot.
