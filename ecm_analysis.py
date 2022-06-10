5fab7d0fd7a73dcbaf783ea2edb9903776e1a19a
Philip <s164254@student.dtu.dk>
2022-06-10 20:00:36 +0200

    titles

--- ecm_analysis.py
+++ ecm_analysis.py
@@ -15,10 +15,14 @@ def analysis_func(exp, samples, protein_group, use_ratio_filter, filter_func):
                       protein_group,
                       use_ratio_filter,
                       filter_func,
-                      title = 't',
-                      xlabel='xl',
-                      ylabel='yl',
+                      title = '',
+                      xlabel='Gene names',
+                      ylabel='Abundance',
                       remove_non_existing=remove_non_existing)
 
 
 ecm.run_analysis(protein_groups, analysis_func)
+
+#Abundance of selected collagen types for the samples
+#Abundance of all laminin proteins for the samples
+#Abundance of fibronectin for the samples
